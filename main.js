f4449da4296e6ff2bb15c7e69ccd4437bafeb68b
A B M Asadullah <abmasadullah@gmail.com>
2022-02-06 14:36:51 +0600

    Start code scaliton

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Hi");
+function computerPlay (playerSelection, computerSelection){
+    if
+        return "Rock";
+    else if
+        return "Paper"
+    else
+        return "Seasor"
+}
