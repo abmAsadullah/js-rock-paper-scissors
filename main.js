e4e75ccedf1c687a50afc9af78876732f7752afc
abmasadullah <abmasadullah@gmail.com>
2022-02-07 11:24:57 +0600

    Complete scalitone

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 function computerPlay(rand) {
     rand = Math.floor((Math.random() * 3) + 1);
-    if(rand == 1) {
-        return "Rock";
-    }
-    else if(rand == 2)
-        return "Paper";
-    else
-        return "Scissor";
+    if(rand == 1) return "Rock";
+    else if(rand == 2) return "Paper";
+    else return "Scissor";
 }
 
-computerSelection = computerPlay();
+// computerSelection = computerPlay();
 // console.log(computerSelection);
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
+    if(playerSelection == "Rock" && computerSelection == "Paper")
+        return "You Loss. Paper bit Rock"
+    if(playerSelection == "Rock" && computerSelection == "Scissor")
+        return "You Win. Rock bit Seassor"
+    if(playerSelection == "Rock" && computerSelection == "Rock")
+        return "Ty game. Rock bit Rock"
+} 
+
+let x = "Rock";
+let y = computerPlay();
+// console.log(y);
+console.log(playRound(x, y));
