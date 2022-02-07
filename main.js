0e1e9d11dc14f162ccd8250f8e4c63a413007f63
abmasadullah <abmasadullah@gmail.com>
2022-02-07 11:04:17 +0600

    Computer section add

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function computerPlay (playerSelection, computerSelection){
-    if
+function computerPlay(rand) {
+    rand = Math.floor((Math.random() * 3) + 1);
+    if(rand == 1) {
         return "Rock";
-    else if
-        return "Paper"
+    }
+    else if(rand == 2)
+        return "Paper";
     else
-        return "Seasor"
+        return "Scissor";
 }
+
+computerSelection = computerPlay();
+// console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
